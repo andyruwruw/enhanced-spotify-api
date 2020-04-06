b71ea4a2100274a2f50828d4eb020ab3ff3e00dd
Andrew Young <andrew@youngshome.com>
2020-04-05 18:04:48 -0700

    Removal of Old Index

--- src/index.js
+++ src/index.js
@@ -9718,4 +9718,44 @@ var EnhancedSpotifyAPI = {
     User: User,
 };
 
+// 'use strict'
+
+// var { addMethods, override } = require('./structures/shared');
+
+// var SpotifyWebAPI = require('spotify-web-api-node');
+
+// function Wrapper() {};
+
+// Wrapper.prototype = {
+//     // Inherits from spotify-web-api-node
+//     ...SpotifyWebAPI.prototype,
+//     // Missing Endpoints
+//     ...require('./functions/Shows'),
+//     ...require('./functions/Episodes'),
+// }
+
+// Wrapper.addMethods = addMethods;
+
+// Wrapper.override = override;
+
+// module.exports = {
+//     Wrapper: Wrapper,
+//     Track: require('./structures/Track'),
+//     Tracks: require('./structures/Tracks'),
+//     Artist: require('./structures/Artist'),
+//     Artists: require('./structures/Artists'),
+//     Album: require('./structures/Album'),
+//     Albums: require('./structures/Albums'),
+//     Playlist: require('./structures/Playlist'),
+//     Playlists: require('./structures/Playlists'),
+//     Category: require('./structures/Category'),
+//     Categories: require('./structures/Categories'),
+//     Show: require('./structures/Show'),
+//     Shows: require('./structures/Shows'),
+//     Episode: require('./structures/Episode'),
+//     Episodes: require('./structures/Episodes'),
+//     Playback: require('./structures/Playback'),
+//     User: require('./structures/User'),
+// };
+
 module.exports = EnhancedSpotifyAPI;
